a911ec9504d32997046617e0bb21ee702b83d15a
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-12-19 23:45:50 +0300

    feat: Added basket and favorites management

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -3,11 +3,16 @@ import { createSlice } from '@reduxjs/toolkit'
 import { MMKV } from 'react-native-mmkv'
 
 const storage = new MMKV()
+
 const favoritesValue = storage.getString('favorites')
 const favorites = favoritesValue ? JSON.parse(favoritesValue) : []
 
+const basketValue = storage.getString('basket')
+const basket = basketValue ? JSON.parse(basketValue) : []
+
 const initialState = {
-  favorites
+  favorites,
+  basket
 }
 
 const slice = createSlice({
@@ -27,11 +32,49 @@ const slice = createSlice({
         state.favorites.push(action.payload)
       }
 
-      return storage.setString('favorites', JSON.stringify(action.payload))
+      return storage.set('favorites', JSON.stringify(state.favorites))
+    },
+    clearAllFavorites: state => {
+      state.favorites = []
+
+      return storage.set('favorites', JSON.stringify(state.favorites))
+    },
+    setBasket: (state, action) => {
+      const { product, type } = action.payload
+
+      const isExist = state.basket.find(item => item.id === product.id)
+
+      if (isExist) {
+        if (type === 'minus') {
+          if (isExist.count > 1) {
+            state.basket = state.basket.map(item => {
+              if (item.id === product.id) {
+                item.count -= 1
+              }
+
+              return item
+            })
+          } else {
+            state.basket = state.basket.filter(item => item.id !== product.id)
+          }
+        } else {
+          state.basket = state.basket.map(item => {
+            if (item.id === product.id) {
+              item.count += 1
+            }
+
+            return item
+          })
+        }
+      } else {
+        state.basket.push({ ...product, count: 1 })
+      }
+
+      return storage.set('basket', JSON.stringify(state.basket))
     }
   }
 })
 
-export const { setFavorites } = slice.actions
+export const { setFavorites, clearAllFavorites, setBasket } = slice.actions
 
 export default slice.reducer
