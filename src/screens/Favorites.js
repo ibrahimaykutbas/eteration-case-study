36ac746704f2a5eeee67399c33e8c04528bb3ae2
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-12-19 23:49:36 +0300

    feat: Added Favorites screen

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,15 +1,61 @@
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  ActionSheetIOS,
+  Platform,
+  Appearance
+} from 'react-native'
 import React from 'react'
 
 import Colors from '../theme/Colors'
 
 import TopMenu from '../components/TopMenu'
+import ProductList from '../components/ProductList'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { clearAllFavorites } from '../redux/slice'
 
 const Favorites = () => {
+  const dispatch = useDispatch()
+
+  const favorites = useSelector(state => state.slice.favorites)
+
+  const handleClearAllFavorites = () => {
+    if (Platform.OS == 'ios') {
+      showActionSheet()
+    } else {
+      dispatch(clearAllFavorites())
+    }
+  }
+
+  const showActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Delete'],
+        destructiveButtonIndex: 1,
+        cancelButtonIndex: 0,
+        userInterfaceStyle: Appearance.getColorScheme()
+      },
+      buttonIndex => {
+        if (buttonIndex === 1) {
+          dispatch(clearAllFavorites())
+        }
+      }
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <TopMenu title="Favorites" />
-      <Text>Favorites</Text>
+      <TopMenu
+        title="Favorites"
+        onPressRight={favorites.length > 0 ? handleClearAllFavorites : null}
+        rightIcon="Trash"
+      />
+
+      <View style={styles.content}>
+        <ProductList products={favorites} />
+      </View>
     </SafeAreaView>
   )
 }
@@ -20,5 +66,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.WHITE
+  },
+  content: {
+    flex: 1
   }
 })
